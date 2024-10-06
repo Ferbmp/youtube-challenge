33457cd38139d63d89b8bdba92346a462711c7fe
Fernando Barreto <fernando200040@hotmail.com>
2024-10-06 18:46:49 -0300

    docs: adding swagger for documentation

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 
 def create_app():
-    app = Flask(__name__)
-    
+    app = Flask(__name__, static_folder='static')
   
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.json'
+    swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL)
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
     from .routes import main_routes
     app.register_blueprint(main_routes)
 
